d22f10d1d9f75b306177eebdbe2c0fdc1cb1bc89
Richard Taylor <richard@softwarelogix.com>
2016-01-11 15:14:21 -0600

    Initial Commit

---
+++ LiveRosary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LiveRosary
+//
+//  Created by richardtaylor on 1/11/16.
+//  Copyright © 2016 Software Logix LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
